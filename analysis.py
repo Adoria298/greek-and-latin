76056b5a55ad3585ce6c432630d35dd8b705175e
Adoria298 <bananaorange29@outlook.com>
2019-10-21 20:23:12 +0100

    Add analysis.py from repl.it.

---
+++ analysis.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+soup = BeautifulSoup(open("docs/the_ethiopians.html", "r").read(), "html.parser")
+
+# count words used
+words = {}
+# find all spans, count the definition and the declension of each word
+for span in soup.find_all("span"):
+	title = span.get("title")
+	if title in words:
+		words[title]['count'] += 1
+		if span.string in words[title]:
+			words[title][span.string] += 1
+		else:
+			words[title][span.string] = 1
+	else:
+		words[title] = {
+			"count": 1
+		}
+		words[title][span.string] = 1
+		
+pprint(words)
