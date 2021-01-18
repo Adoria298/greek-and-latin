df0bdbe2c54f3a90532d4c2c493d7dc5f8ac43b8
adoria298 <bananaorange29@outlook.com>
2021-01-18 16:06:46 +0000

    made make_vocab_table importable

--- make_vocab_table.py
+++ make_vocab_table.py
@@ -2,10 +2,6 @@ from bs4 import BeautifulSoup
 
 from pathlib import Path
 
-src_path = Path(input("Please input the document you'd like to read. "))
-with src_path.open(mode='r', encoding='utf-8') as src: # trusting the user
-    srcy_soup = BeautifulSoup(src.read(), "html.parser")
-
 def get_vocab_items(soup):
     "Takes in a BeautifulSoup and returns a dictionary of word (str): definition (str)"
     vocab_dict = {}
@@ -13,3 +9,10 @@ def get_vocab_items(soup):
         word, definition = span.get("title").split(" - ")
         vocab_dict[word] = definition
     return vocab_dict
+
+if __name__ == "__main__":
+    src_path = Path(input("Please input the document you'd like to read. "))
+    with src_path.open(mode='r', encoding='utf-8') as src: # trusting the user
+        srcy_soup = BeautifulSoup(src.read(), "html.parser")
+
+    vocab = get_vocab_items(soup)
